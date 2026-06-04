3cc00e9ba3c36944763a2930960a8961f6dbd26c
Ruth LONTSIE <ruthlontsie20@gmail.com>
2026-06-04 07:48:23 +0100

    Fix URL Gemini v1beta flash-latest

--- main.py
+++ main.py
@@ -26,7 +26,8 @@ app.add_middleware(
 
 # On utilise l'URL v1 (la plus stable)
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-GEMINI_URL = f"https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key={GEMINI_API_KEY}"
+# GEMINI_URL = f"https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key={GEMINI_API_KEY}"
+GEMINI_URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key={GEMINI_API_KEY}"
 
 # ─────────────────────────────────────────────────────────────────
 # SYSTEM PROMPT DE BASE
